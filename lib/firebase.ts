13b192e7c3f7ba522a5a0a3a5279d190a7994cd3
engkalbali <eng.kalbali@gmail.com>
2025-05-25 08:13:04 +0330

    5/25 8:12am

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,26 @@
 // lib/firebase.ts
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDnh1EUPJfAX1DznSoJqO4nVNyjnN1uXKM",
-    authDomain: "helizo-8594.firebaseapp.com",
-    projectId: "helizo-8594",
-    storageBucket: "helizo-8594.firebasestorage.app",
-    messagingSenderId: "576210292113",
-    appId: "1:576210292113:web:bea94510a621f68cb61e30",
-    measurementId: "G-ZEH7DCQPBC"
+  apiKey: "AIzaSyDnh1EUPJfAX1DznSoJqO4nVNyjnN1uXKM",
+  authDomain: "helizo-8594.firebaseapp.com",
+  projectId: "helizo-8594",
+  storageBucket: "helizo-8594.firebasestorage.app",
+  messagingSenderId: "576210292113",
+  appId: "1:576210292113:web:bea94510a621f68cb61e30",
+  measurementId: "G-ZEH7DCQPBC",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// ✅ استفاده از AsyncStorage برای حفظ لاگین بین جلسات
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+const db = getFirestore(app);
+
+export { auth, db };
